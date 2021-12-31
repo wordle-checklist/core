231ceb27dad4fb9555ac8bfb6b29eb073504ffe7
Sam Tuson <sam.tuson@hotmail.co.uk>
2021-12-31 19:43:10 +0000

    Write results to file

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const multer = require("multer");
 const mime = require("mime-types");
 const path = require("path");
+const fs = require("fs");
+
+const today = () => {
+    return new Date().toISOString().split("T")[0];
+};
 
 const app = express();
 app.use(express.static("public", { extensions: ["html"] }));
@@ -9,9 +14,8 @@ app.use(express.static("public", { extensions: ["html"] }));
 const storage = multer.diskStorage({
     destination: "img",
     filename: (req, file, cb) => {
-        const date = new Date().toISOString().split("T")[0];
         const ext = mime.extension(file.mimetype);
-        cb(null, `${date}_${file.fieldname}_${req.body.name}.${ext}`);
+        cb(null, `${today()}_${file.fieldname}_${req.body.name}.${ext}`);
     },
 });
 
@@ -39,6 +43,14 @@ const upload = multer({ storage, limits, fileFilter }).fields([
 ]);
 
 const addResult = (req, res) => {
+    const filename = `${today()}_results_${req.body.name}`;
+    const { name, score } = req.body;
+    const { guesses, stats } = req.files;
+    const guessesFilename = guesses ? guesses[0].filename : "";
+    const statsFilename = stats ? stats[0].filename : "";
+    const csv = [name, score, guessesFilename, statsFilename].join(",");
+    fs.writeFileSync(`results/${filename}`, csv);
+
     res.redirect("results");
 };
 
